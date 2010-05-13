53c0d9ca90344a7cb69c3ffb39a2b7dd3a5b8580
lynn <lynn.atg@gmail.com>
2010-05-13 10:08:52 +0800

    test commit add signed-off-by

---
+++ src/com/test/CopyOfStudent.java
@@ -0,0 +1,53 @@
+package com.test;
+
+public class CopyOfStudent
+{
+    private String _number2;
+    private String _name2;
+    private int _score;
+
+    public CopyOfStudent()
+    {
+        _number2 = null;
+        _name2 = null;
+        _score = 0;
+    }
+
+    public CopyOfStudent(String num, String name, int score)
+    {
+        _number2 = num;
+        _name2 = name;
+        _score = score;
+    }
+
+    public String getNumber2()
+    {
+        return _number2;
+    }
+
+    public String getName2()
+    {
+        return _name2;
+    }
+
+    public int getScore()
+    {
+        return _score;
+    }
+
+    public void setNumber(String num)
+    {
+        _number2 = num;
+    }
+
+    public void setName(String name)
+    {
+        _name2 = name;
+    }
+
+    public void setScore(int score)
+    {
+        _score = score;
+    }
+
+}
